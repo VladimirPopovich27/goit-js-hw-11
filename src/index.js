8daf0cc1f2170a5721c5425cb3ca2d07987aa758
VladimirPopovich27 <117470833+VladimirPopovich27@users.noreply.github.com>
2023-04-23 11:58:59 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,122 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { FetcherOfImages } from './js/fetchImages';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-notify-aio-3.2.6.min.js';
+
+const searchFormRef = document.querySelector('.search-form');
+const galleryRef = document.querySelector('.gallery');
+
+const loadMoreBtn = document.querySelector('.load-more');
+const observer = new IntersectionObserver(intersectingHandler);
+observer.observe(loadMoreBtn);
+
+searchFormRef.addEventListener('submit', onSubmitForm);
+
+const fetcherOfImages = new FetcherOfImages();
+const simpleLightbox = initializeSimpleLightbox();
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  fetchImages();
+}
+
+async function fetchImages() {
+  const query = searchFormRef.elements.searchQuery.value.trim();
+  if (query === '') {
+    return;
+  }
+  await fetcherOfImages
+    .getImages(query)
+    .then(({ data }) => {
+      if (data.hits.length === 0) {
+        galleryRef.innerHTML = '';
+        return Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      if (fetcherOfImages.page === 1) {
+        galleryRef.innerHTML = renderGallery(data.hits);
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      } else {
+        galleryRef.insertAdjacentHTML('beforeend', renderGallery(data.hits));
+        setScrollbehavior();
+      }
+    })
+    .catch(error => {
+      if (error.response.status === 400) {
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        Notify.failure('Something went wrong. Try again!');
+      }
+    });
+  simpleLightbox.refresh();
+}
+
+function initializeSimpleLightbox() {
+  return new SimpleLightbox('.gallery a');
+}
+
+function renderGallery(images) {
+  return images
+    .map(
+      ({
+        webformatURL: preview,
+        largeImageURL: original,
+        tags: description,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+                    <a class="gallery__link" href="${original}">
+                        <img src="${preview}" alt="${description}" loading="lazy" />
+                        <div class="info">
+                            <p class="info-item">
+                            <b>Likes</b>
+                            ${likes}
+                            </p>
+                            <p class="info-item">
+                            <b>Views</b>
+                            ${views}
+                            </p>
+                            <p class="info-item">
+                            <b>Comments</b>
+                            ${comments}
+                            </p>
+                            <p class="info-item">
+                            <b>Downloads</b>
+                            ${downloads}
+                            </p>
+                        </div>
+                    </a>
+                 </div>`;
+      }
+    )
+    .join('');
+}
+
+function setScrollbehavior() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function intersectingHandler(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      fetchImages();
+    }
+  });
+}
